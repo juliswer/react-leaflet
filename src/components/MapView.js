cc672a45f970abc4e2b170ce4a9e612d2f05b42d
Julián Swerdlin <julianswer36@gmail.com>
2021-07-11 12:32:25 -0300

    new files

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import { MapContainer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
 const MapView = () => {
+
+    const position = [51.505, -0.09];
+
     return (
-        <h1>Map</h1>
+        <MapContainer center={position} zoom={13} >
+
+        </MapContainer>
     )
 }
 
